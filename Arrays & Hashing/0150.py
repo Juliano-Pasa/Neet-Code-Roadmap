a556070fac42b41f9d27e9f6b90151d575f4c8c6
Juliano Pasa <julianodemellopasa@gmail.com>
2025-02-01 18:31:13 -0300

    Problem 150

---
+++ Arrays & Hashing/0150.py
@@ -0,0 +1,11 @@
+from typing import List
+
+class Solution:
+    def hasDuplicate(self, nums: List[int]) -> bool:
+        appeared = set([])
+
+        for num in nums:
+            if num in appeared:
+                return True
+            appeared.add(num)
+        return False
