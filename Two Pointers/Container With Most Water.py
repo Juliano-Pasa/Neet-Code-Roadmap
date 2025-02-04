956f2302cb23e6c20d1509e4d39253134fd907f7
Juliano Pasa <julianodemellopasa@gmail.com>
2025-02-03 21:21:17 -0300

    Container With Most Water

---
+++ Two Pointers/Container With Most Water.py
@@ -0,0 +1,20 @@
+from typing import List
+
+class Solution:
+    def maxArea(self, heights: List[int]) -> int:
+        maximum = 0
+        left = 0
+        right = len(heights) - 1
+
+        while left < right:
+            area = min(heights[left], heights[right]) * (right - left)
+
+            if area > maximum:
+                maximum = area
+
+            if heights[left] < heights[right]:
+                left += 1
+            else:
+                right -= 1
+
+        return maximum
